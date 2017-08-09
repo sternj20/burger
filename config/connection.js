8a06fb42e5d76261665453175c016b8c3d936e93
sternj20 <jessesternmusic@gmail.com>
2017-08-09 14:50:09 -0700

    push to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,14 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var path = require("path");
+var keys = require(path.join(__dirname, "keys.json"));
 
 var connection;
 
 if(process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  connection = mysql.createConnection({
-    port: 3306,
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "burgers_db"
-  });
+  connection = mysql.createConnection(keys);
 }
 
 // Make connection.

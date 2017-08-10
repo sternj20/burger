fb75db484fbff28aeb179cc47f2c158bf70ddc10
sternj20 <jessesternmusic@gmail.com>
2017-08-10 14:05:17 -0700

    pushing to heroku

--- config/connection.js
+++ config/connection.js
@@ -9,12 +9,12 @@ if(process.env.JAWSDB_URL) {
 } else{
 	connection = mysql.createConnection({
 		root:3000,
-		host: 'b4e9xxkxnpu2v96i.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-		user: 'jxwbiwgj83jodyc1',
-		password: 'uk0adq157izbg776',
-		database: 'y38btxsc1x5q6fv4'
+		host: 'localhost',
+		user: root,
+		password: '',
+		database: 'burgers_db'
 	});
-}
+};
 
 // Make connection.
 connection.connect(function(err) {

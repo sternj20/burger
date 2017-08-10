078f41dc7f6c531e05f644bc15d240f150ea2929
sternj20 <jessesternmusic@gmail.com>
2017-08-10 10:20:05 -0700

    trying to replace connection else statement see if this works with heroku

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,15 @@ var connection;
 
 if(process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
-} 
+} else{
+	connection = mysql.createConnection({
+		root:3000,
+		host: 'localhost',
+		user: root,
+		password: '',
+		database: 'burgers_db'
+	});
+};
 
 // Make connection.
 connection.connect(function(err) {
